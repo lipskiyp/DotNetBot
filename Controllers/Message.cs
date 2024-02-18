976ba869ef17b356ad683da9567f976acbc1326b
Pavel Lipskiy <Pavel@MacBook-Pro-Pavel.local>
2024-02-18 20:45:01 +0600

    messages crud

--- Controllers/Message.cs
+++ Controllers/Message.cs
@@ -20,8 +20,59 @@ public class MessageController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Message>>> GetMessages()
+    public async Task<ActionResult<List<Message>>> ListMessages()
     {
         return await _context.Messages.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Message>> GetMessage(int id)
+    {
+        var message = await _context.Messages.FindAsync(id);
+        if (message == null)
+            return NotFound();
+
+        return message;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Message>> CreateMessage(Message message)
+    {
+        message.CreatedAt = DateTime.UtcNow;
+        message.UpdatedAt = DateTime.UtcNow;
+
+        _context.Messages.Add(message);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetMessage", new { id = message.Id }, message);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateMessage(int id, Message message)
+    {
+        if (id != message.Id)
+            return BadRequest();
+
+        var _message = await _context.Messages.FindAsync(id);
+        if (_message == null)
+            return NotFound();
+
+        _context.Entry(message).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMessage(int id)
+    {
+        var message = await _context.Messages.FindAsync(id);
+        if (message == null)
+            return NotFound();
+
+        _context.Messages.Remove(message);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-### DotNet API with Telegram Bot
+# DotNet API with Telegram Bot
 
 DotNet practice project that implements a Telegram API client with basic DotNet CRUD API to store/retrieve messages.
+
+## Launch
+
+```cs
+dotnet run
+```
+
+## API Docs
+
+http://localhost:5031/swagger/index.html
